722c9aa835b132be41c3460c7f0dfd62d8639b81
Blackjack14C <800900@sylvaniastudents.org>
2023-10-13 10:23:55 -0400

    Finish Scripts

--- PlayerControllerX.cs
+++ PlayerControllerX.cs
@@ -17,6 +17,7 @@ public class PlayerControllerX : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
@@ -24,6 +25,6 @@ public class PlayerControllerX : MonoBehaviour
         transform.Translate(Vector3.forward * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime * verticalInput);
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * verticalInput);
     }
 }
